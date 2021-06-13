de30b2bae0ec82ac02f5aaca0cc08c762897d6bb
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-12 23:45:11 -0500

    added subset_without_x_and_2x.py and its base code

---
+++ subset_without_x_and_2x.py
@@ -0,0 +1,12 @@
+############------------ IMPORTS ------------############
+from itertools import combinations
+
+
+############------------ FUNCTIONS ------------############
+def subset_without_x_and_2x():
+    pass
+
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    pass
