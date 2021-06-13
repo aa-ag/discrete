a905a5fcf46550ecc32166296dce65c23e3ff203
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-13 06:49:47 -0500

    added getting_cash_change.py

---
+++ getting_cash_change.py
@@ -0,0 +1,18 @@
+############------------ FUNCTIONS ------------############
+def change(amount):
+    assert(amount >= 8)
+    if amount == 8:
+        return [3, 5]
+    if amount == 9:
+        return [3, 3, 3]
+    if amount == 10:
+        return [5, 5]
+
+    coins = change(amount - 3)
+    coins.append(3)
+    return coins
+
+
+############------------ DRIVER CODE ------------############
+if __name__ == '__main__':
+    print(change(11))
--- subset_without_x_and_2x.py
+++ subset_without_x_and_2x.py
@@ -4,9 +4,16 @@ from itertools import combinations
 
 ############------------ FUNCTIONS ------------############
 def subset_without_x_and_2x():
-    pass
+    '''
+     Choose the maximal number of integers 
+     among 1..50 
+     that can be selected if we are not allowed 
+     to select n and 2n at at same time.
+    '''
+    integers = [i for i in range(1, 51)]
+    return len(list(j for j in combinations(integers, 2) if j[1] < (j[0] * 2)))
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    pass
+    print(subset_without_x_and_2x())
