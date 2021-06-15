b0a671e087adc18e322280726a826a1502ff5243
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-15 18:50:54 -0500

    added logic from class to hanoi.py

--- problems/hanoi.py
+++ problems/hanoi.py
@@ -1,10 +1,19 @@
 # source of this implementation: https://www.youtube.com/watch?v=0u7g9C0wSIA
 ############------------ FUNCTIONS ------------############ 
-def print_move():
-    pass
+def print_move(from_tower, to_tower):
+    print(f"move {str(from_tower)} to {str(to_tower)}")
 
-def towers():
-    pass
+def towers(n, from_tower, to_tower, spare_tower):
+    # assert that there is more than zero rings 
+    assert(n > 0)
+
+    # base case
+    if n == 1:
+        print_move(from_tower, to_tower)
+    else:
+        towers(n - 1, from_tower, to_tower, spare_tower)
+        towers(1, from_tower, to_tower, spare_tower)
+        towers(n - 1, spare_tower, to_tower, from_tower)
 
 
 ############------------ DRIVER CODE ------------############
