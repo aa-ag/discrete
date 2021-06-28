d23e2e8f167de5ab646dc863c6029d7bdfc09c98
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-28 18:22:45 -0500

    infinitely iterating over 1, 2 & 3 using cycle() in codewars.py

--- problems/codewars.py
+++ problems/codewars.py
@@ -1,17 +1,14 @@
 ############------------ IMPORTS ------------############
 from itertools import cycle
 
+
 ############------------ FUNCTION(S) ------------############
 def cycle_through_an_iterable(iterable):
-    saved = list()
-    for element in iterable:
-        yield element
-        saved.append(element)
-    while saved:
-        for element in saved:
-              yield element
+    for i in iterable:
+        print(i)
 
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    cycle_through_an_iterable('ABCD')
+    iterable = cycle([1,2,3])
+    cycle_through_an_iterable(iterable)
