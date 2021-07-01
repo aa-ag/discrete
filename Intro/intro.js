ed241d2572912ff63ec695b1882b9082babaf45d
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-01 18:39:49 -0500

    added script to add gif right after h1 element

--- Intro/intro.js
+++ Intro/intro.js
@@ -6,3 +6,11 @@ for (var i = 0; i < 10; i++) {
 // restyling one dom element
 document.getElementsByTagName("h1")[0].style.color = "#ff0000";
 
+// adding gif in page at click
+document.body.addEventListener('click', function () {
+    var myParent = document.getElementsByTagName("h1")[0]; 
+    var myImage = document.createElement("img");
+    myImage.src = 'https://thecatapi.com/api/images/get?format=src&type=gif';
+    myParent.appendChild(myImage);
+    myImage.style.marginLeft = "160px";
+});
