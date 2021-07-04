0ad6bdffdb3d7c1edb1d31e48598cf1008aacfad
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-03 20:16:07 -0500

    added function titleCase in strings.js

--- Intro/quizzes/strings.js
+++ Intro/quizzes/strings.js
@@ -1,3 +1,25 @@
 // slicing 
 var quote = "Stay awhile and listen!";
 console.log(quote[6]);
+
+// favorite food
+/*
+ * Programming Quiz: Favorite Food (2-3)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should print a string with your favorite food
+ * 2. Your code should have the first character capitalized
+ * 3. Your code must have a `console.log()` function
+ * 4. Your code should not be empty
+ */
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+    return str.join(' ');
+  }
+
+  console.log(titleCase('water'));
