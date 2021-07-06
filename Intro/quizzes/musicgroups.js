b9bd0beafa1d7d32ec06ce757b064c28f4a22be1
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-06 17:30:32 -0500

    added musicgroups.js

---
+++ Intro/quizzes/musicgroups.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Musical Groups (3-3)
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have a variable `musicians`, and include `if...else if...else` conditional statement
+ * 2. Your code should produce the expected output, as mentioned above. Read each condition carefully. 
+ */
+ 
+// change the value of `musicians` to test your conditional statements
+var musicians = 1;
+
+// your code goes here
