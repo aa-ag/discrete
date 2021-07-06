b8072cfe141e82d1319ddf186ffe07a3a1e74ee2
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-06 17:40:41 -0500

    added logic for musicgroups.js: 5 else ifs: passing all tests in quizz

--- Intro/quizzes/musicgroups.js
+++ Intro/quizzes/musicgroups.js
@@ -11,3 +11,16 @@
 var musicians = 1;
 
 // your code goes here
+if (musicians <= 0) {
+    console.log("not a group");
+} else if (musicians === 1) {
+    console.log("solo");
+} else if (musicians === 2) {
+    console.log("duet");
+} else if (musicians === 3) {
+    console.log("trio");
+} else if (musicians === 4) {
+    console.log("quartet");
+} else if (musicians >= 5) {
+    console.log("this is a large group");
+}
