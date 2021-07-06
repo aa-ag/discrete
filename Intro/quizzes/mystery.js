a26151a243ee45911e4402424e4677d8096da713
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-06 17:53:41 -0500

    fixed typos and confusing logic, own code now passing all quizz tests

--- Intro/quizzes/mystery.js
+++ Intro/quizzes/mystery.js
@@ -23,60 +23,27 @@ We also know that each weapon corresponds to a particular room, so...
 * and the knife belongs to the dining room.
 
 /* ****************************************** */
-/* ****************************************** */
-/* TESTING LOGIC */
-// Change the value of `room` and `suspect` to test your code
-// A room can be either of - dining room, gallery, ballroom, or billiards room
 var room = "billiards room";
-
-// A suspect can be either of - Mr. Parkes, Ms. Van Cleve, Mrs. Sparr, or Mr. Kalehoff
 var suspect = "Mr. Parkes";
-
-/* ****************************************** */
-
-/* IMPLEMENTATION LOGIC*/
 var weapon = "";  // Initial value
-
-// A boolean variable `solved` will tell us the status of mystery
-// true --> solved and false --> unsolved
 var solved = false;
 
-
-/*
-* To help solve this mystery, write a combination of conditional statements that:
-* 1. sets the value of weapon based on the room and
-* 2. sets the value of solved to true if the value of room matches the suspect's room
-*/
-if (room === 'ballroom') {
-    weapon = 'poison';
-    // Mr. Kalehoff was present in the ballroom at the time of the murder
-    if(suspect==="Mr. Kalehoff") 
-        solved = true;
-}
-/* ANOTHER WAY OF CHECKING THE CONDITION
 if (room === 'ballroom' && suspect==="Mr. Kalehoff") {
     weapon = 'poison';
     solved = true;
 }
-We suggest you to try this way of checking the condition as well for an unsolved mystery.
-*/
-else if (room === 'gallery') {
+
+else if (room === 'gallery' && suspect==="Ms. Van Cleve") {
     weapon = 'trophy';
-    // Ms. Van Cleve was present in the gallery at the time of the murder
-    if(suspect==="Ms. Van Cleve") 
-        solved = true;
+    solved = true;
 }
-else if (room === 'billiards room') {
+else if (room === 'billiards room' && suspect==="Mrs. Sparr") {
     weapon = 'pool stick';
-    // Mrs. Sparr was present in the billiards room at the time of the murder
-    if(suspect==="Mrs. Sparr") 
-        solved = true;
+    solved = true;
 } 
-else if (room === 'dining room') {
+else if (room === 'dining room' && suspect==="Mr. Parkes") {
     weapon = 'knife';
-    // Mr. Parkes was present in the dining room at the time of the murder
-    if(suspect==="Mr. Parkes") 
-        solved = true;
+    solved = true;
 }   
 
 /* ****************************************** */
