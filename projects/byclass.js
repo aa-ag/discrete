974c3f9a011be1cbbfe1e751f3a602cf02554ebb
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-08 18:48:21 -0500

    added two more tests, seems to be working fine, as long as there is a DOD in the obit

--- projects/byclass.js
+++ projects/byclass.js
@@ -1,5 +1,12 @@
-// https://www.legacy.com/us/obituaries/chicagotribune/name/betty-barnum-obituary?id=5931983
-
 // 1 > attributes > childNodes > 0 > textContent
 var node = document.getElementsByClassName('Box-sc-5gsflb-0 Text-qbt5xm-0 dDhReN')[1];
 console.log(node.textContent)
+
+// https://www.legacy.com/us/obituaries/chicagotribune/name/betty-barnum-obituary?id=5931983
+// July 4, 2021
+
+// https://www.legacy.com/us/obituaries/chicagotribune/name/anthony-christofaro-obituary?id=5935828
+// 2021
+
+// https://www.legacy.com/us/obituaries/chicagotribune/name/michael-callahan-obituary?id=5805149
+// 2021
