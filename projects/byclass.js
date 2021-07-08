19f2396cc6d23dbe24d107da1d55d931f54014a5
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-08 18:38:00 -0500

    added byclass.js and logic to grab specific element by class form given url, navigated object and found path to data

---
+++ projects/byclass.js
@@ -0,0 +1,6 @@
+// https://www.legacy.com/us/obituaries/chicagotribune/name/betty-barnum-obituary?id=5931983
+
+var node = document.getElementsByClassName('Box-sc-5gsflb-0 Text-qbt5xm-0 dDhReN');
+console.log(node)
+
+// 1 > attributes > childNodes > 0 > textContent
