6f2f53044f44bfa6c0b88f7b1877d31dabaf4e34
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-08 18:41:10 -0500

    getting specific object from list of object with that class in byclass.js

--- projects/byclass.js
+++ projects/byclass.js
@@ -1,6 +1,6 @@
 // https://www.legacy.com/us/obituaries/chicagotribune/name/betty-barnum-obituary?id=5931983
 
-var node = document.getElementsByClassName('Box-sc-5gsflb-0 Text-qbt5xm-0 dDhReN');
+var node = document.getElementsByClassName('Box-sc-5gsflb-0 Text-qbt5xm-0 dDhReN')[1];
 console.log(node)
 
 // 1 > attributes > childNodes > 0 > textContent
