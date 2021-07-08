4ff3ffe07b10cf13c1e551240046a0f96ca28e65
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-08 18:06:34 -0500

    added another version for the url version using  to avoid Firefox bug

--- projects/url.js
+++ projects/url.js
@@ -1,2 +1,13 @@
+// this works, with documented issues
+// in Firefox
 url = document.URL
 console.log(url)
+
+
+// version which works in Firefox too
+url = window.location.href
+console.log(url)
+
+// https://stackoverflow.com/questions/1034621/get-the-current-url-with-javascript
+// this thread includes several options
+// and discussions
