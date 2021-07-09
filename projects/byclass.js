22ae196be3006bcdad913325b45ef22b8bd2f78b
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-08 19:07:51 -0500

    added logic to grab year from dat published from notice in byclass.js

--- projects/byclass.js
+++ projects/byclass.js
@@ -10,3 +10,11 @@ console.log(node.textContent)
 
 // https://www.legacy.com/us/obituaries/chicagotribune/name/michael-callahan-obituary?id=5805149
 // 2021
+
+
+var node = document.getElementsByClassName("Box-sc-5gsflb-0 Text-qbt5xm-0 kLoMdS")[0];
+var string = node.innerText.split(' ');
+console.log(string.slice(-1)[0].slice(0, -1))
+
+// https://www.legacy.com/us/obituaries/chicagotribune/name/gerard-bungert-obituary?id=5854568
+// 2021
