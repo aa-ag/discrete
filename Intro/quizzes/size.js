304762731540ecb66997c033d2564261efede1cc
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-11 10:12:21 -0500

    added logic for NA results from class

--- Intro/quizzes/size.js
+++ Intro/quizzes/size.js
@@ -40,22 +40,36 @@
 */
 
 // change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
-var shirtWidth = 23;
-var shirtLength = 30;
-var shirtSleeve = 8.71;
+var shirtWidth = 9.99;
+var shirtLength = 28.99;
+var shirtSleeve = 8.379;
 
 // WRITE YOUR CODE HERE
+var shirtWidth = 21;
+var shirtLength = 99;
+var shirtSleeve = 8.40;
 
-if (shirtWidth <= 18 && shirtLength <= 28 && shirtSleeve <= 8.13) {
-    console.log('S')
-} else if (shirtWidth <= 20 && shirtLength <= 29 && shirtSleeve <= 8.38) {
-    console.log('M')
-} else if (shirtWidth <= 22 && shirtLength <= 30 && shirtSleeve <= 8.63) {
-    console.log('L')
-} else if (shirtWidth <= 24 && shirtLength <= 31 && shirtSleeve <= 8.88) {
-    console.log('XL')
-} else if (shirtWidth <= 26 && shirtLength <= 33 && shirtSleeve <= 9.63) {
-    console.log('2XL')
-} else if (shirtWidth <= 28 && shirtLength <= 24 && shirtSleeve <= 10.13) {
-    console.log('3XL')
-} 
+var size = "NA";
+
+if ((shirtWidth>=18 && shirtWidth<20) && (shirtLength>=28 && shirtLength<29) && (shirtSleeve>=8.13 && shirtSleeve<8.38) ) {
+    size = "S";
+}
+else if ((shirtWidth>=20 && shirtWidth<22) && (shirtLength>=29 && shirtLength<30) && (shirtSleeve>=8.38 && shirtSleeve<8.63) ) {
+    size = "M";
+}
+else if ((shirtWidth>=22 && shirtWidth<24) && (shirtLength>=30 && shirtLength<31) && (shirtSleeve>=8.63 && shirtSleeve<8.88) ) {
+    size = "L";
+}
+else if ((shirtWidth>=24 && shirtWidth<26) && (shirtLength>=31 && shirtLength<33) && (shirtSleeve>=8.88 && shirtSleeve<9.63) ) {
+    size = "XL";
+}
+else if ((shirtWidth>=26 && shirtWidth<28) && (shirtLength>=33 && shirtLength<34) && (shirtSleeve>=9.63 && shirtSleeve<10.13) ) {
+    size = "2XL";
+}
+else if ((shirtWidth>=28) && (shirtLength>=34) && (shirtSleeve>=10.13) ) {
+    size = "3XL";
+}
+else {
+    size = "NA";
+}
+console.log(size);
