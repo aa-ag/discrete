38141f21f0d100d32f8215e943001d34eebd3a39
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-11 10:09:02 -0500

    added own version of size.js conditionals

--- Intro/quizzes/size.js
+++ Intro/quizzes/size.js
@@ -25,13 +25,6 @@
  * 6. Your code should not be empty
  * 7. BE CAREFUL ABOUT THE EXACT CHARACTERS TO BE PRINTED.
  */
- 
-
-
-// change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
-var shirtWidth = 23;
-var shirtLength = 30;
-var shirtSleeve = 8.71;
 
 /*
  * To gain confidence, check your code for the following combination of [shirtWidth, shirtLength, shirtSleeve, expectedSize]:
@@ -46,4 +39,23 @@ var shirtSleeve = 8.71;
  * [18, 29, 8.47, 'NA']
 */
 
+// change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
+var shirtWidth = 23;
+var shirtLength = 30;
+var shirtSleeve = 8.71;
+
 // WRITE YOUR CODE HERE
+
+if (shirtWidth <= 18 && shirtLength <= 28 && shirtSleeve <= 8.13) {
+    console.log('S')
+} else if (shirtWidth <= 20 && shirtLength <= 29 && shirtSleeve <= 8.38) {
+    console.log('M')
+} else if (shirtWidth <= 22 && shirtLength <= 30 && shirtSleeve <= 8.63) {
+    console.log('L')
+} else if (shirtWidth <= 24 && shirtLength <= 31 && shirtSleeve <= 8.88) {
+    console.log('XL')
+} else if (shirtWidth <= 26 && shirtLength <= 33 && shirtSleeve <= 9.63) {
+    console.log('2XL')
+} else if (shirtWidth <= 28 && shirtLength <= 24 && shirtSleeve <= 10.13) {
+    console.log('3XL')
+} 
