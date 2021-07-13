fd29b87ad19eef1e5254dc7cd1e53ae00aceda39
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-13 18:42:42 -0500

    fixed string typo in console.log string, passing all tests

--- Intro/quizzes/school.js
+++ Intro/quizzes/school.js
@@ -24,30 +24,30 @@ a Professional degree earned an average of $89,960/year,
 and a Doctoral degree earned an average of $84,396/year.
 */
 
-var salary = '';
+var salary = 0;
 
 switch (education) {
   case 'no high school diploma':
-    salary += "25,636";
+    salary = 25636;
     break;
   case 'a high school diploma':
-    salary += "35,256";
+    salary = 35256;
     break;
   case 'an Associate\'s degree':
-    salary += "41,496";
+    salary = 41496;
     break;
   case 'a Bachelor\'s degree':
-    salary += "59,124";
+    salary = 59124;
     break;
   case 'a Master\'s degree':
-    salary += "69,732";
+    salary = 69732;
     break;
   case 'a Professional degree':
-    salary += "89,960";
+    salary = 89960;
     break;
   case 'a Doctoral degree':
-    salary += "84,396";
+    salary = 84396;
     break;
 }
 
-console.log("In 2015, a person with " + education + " degree earned an average of $" + salary + "/year.");
+console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year.");
