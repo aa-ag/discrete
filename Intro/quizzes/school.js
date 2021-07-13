4ea919d7261a365882f420ba1a2f7cdfa05567f8
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-13 18:12:13 -0500

    added school.js with its base code from class/quizz

---
+++ Intro/quizzes/school.js
@@ -0,0 +1,23 @@
+/*
+ * Programming Quiz: Back to School (3-9)
+ *
+ * Write a switch statement to set the average salary of a person based on their type of completed education.
+ *
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have the variables `education`, and `salary`
+ * - Your code should include a switch statement
+ * - Your code should produce the expected output
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+ 
+// change the value of `education` to test your code
+var education = 'no high school diploma';
+
+// set the value of this based on a person's education
+var salary = 0;
+
+// your code goes here
