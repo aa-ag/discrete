73b2fed099cd92b59a823d1be6aff2cff22762b2
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-06 21:39:50 -0500

    added countdown.js quizz and its intructions from udacity class

---
+++ Intro/quizzes/countdown.js
@@ -0,0 +1,24 @@
+/*
+ * Programming Quiz: Countdown, Liftoff! (4-3)
+ * 
+ * Using a while loop, print out the countdown output above.
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should include a `while` loop
+ * - Your `while` loop should have a stop condition
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+
+// your code goes here
+
+// Orbiter transfers from ground to internal power (T-50 seconds)
+// Ground launch sequencer is go for auto sequence start (T-31 seconds)
+// Activate launch pad sound suppression system (T-16 seconds)
+// Activate main engine hydrogen burnoff system (T-10 seconds)
+// Main engine start (T-6 seconds)
+// Solid rocket booster ignition and liftoff! (T-0 seconds)
