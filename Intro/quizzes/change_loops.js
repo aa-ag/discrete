ddc611364a14405b417defc96e560bde2c24a8a9
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-07 12:45:21 -0500

    version of change_loops.js passing all tests

--- Intro/quizzes/change_loops.js
+++ Intro/quizzes/change_loops.js
@@ -13,12 +13,6 @@
  */
  
 // rewrite the while loop as a for loop
-var x = 9;
-while (x >= 1) {
+for (var x = 9; x >= 1; x--) {
     console.log("hello " + x);
-    x = x - 1;
-}
-
-for (var x = 9; x > 0; x = x - 1) {
-    console.log("Printing out x = " + x);
 }
