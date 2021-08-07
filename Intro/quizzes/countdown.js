91ab3315553eba3d5d9d735fb296b242ed36d2d0
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-07 12:22:36 -0500

    refactored to === for compares

--- Intro/quizzes/countdown.js
+++ Intro/quizzes/countdown.js
@@ -25,22 +25,22 @@
 let t = 60
 
 while (t >= 0) {
-    if (t == 50) {
+    if (t === 50) {
         console.log("Orbiter transfers from ground to internal power")
         t -= 1
-    } else if (t == 31) {
+    } else if (t === 31) {
         console.log("Ground launch sequencer is go for auto sequence start")
         t -= 1
-    } else if (t == 16) {
+    } else if (t === 16) {
         console.log("Activate launch pad sound suppression system")
         t -= 1
-    } else if (t == 10) {
+    } else if (t === 10) {
         console.log("Activate main engine hydrogen burnoff system")
         t -= 1
-    } else if (t == 6) {
+    } else if (t === 6) {
         console.log("Main engine start")
         t -= 1
-    }  else if (t == 0) {
+    }  else if (t === 0) {
         console.log("Solid rocket booster ignition and liftoff!")
         t -= 1
     } else {
